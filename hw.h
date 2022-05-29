af09c2667d1345eb8fb686544f176b4fdab2736a
kingyo <kingyo.no.esa1192@gmail.com>
2022-05-29 22:24:23 +0900

    add hw.h

---
+++ hw.h
@@ -0,0 +1,44 @@
+#ifndef __HW_H__
+#define __HW_H__
+
+// Photocoupler
+#define DEF_PINNUM_PC0          (0)
+#define DEF_PINNUM_PC1          (1)
+#define DEF_PINNUM_PC2          (18)
+#define DEF_PINNUM_PC3          (17)
+
+// SFP module
+#define DEF_PINNUM_SFP_RX       (2)
+#define DEF_PINNUM_SFP_TX       (3)
+#define DEF_PINNUM_SFP_TX_FAULT (4)
+#define DEF_PINNUM_SFP_RX_LOS   (6)
+#define DEF_PINNUM_SFP_SCL      (5)
+#define DEF_PINNUM_SFP_SDA      (19)
+
+// ADC
+#define DEF_PINNUM_AD1_SCK      (15)
+#define DEF_PINNUM_AD1_SDO      (14)
+#define DEF_PINNUM_AD1_CONV     (13)
+#define DEF_PINNUM_AD2_SCK      (10)
+#define DEF_PINNUM_AD2_SDO      (11)
+#define DEF_PINNUM_AD2_CONV     (12)
+#define DEF_PINNUM_AD3_SCK      (7)
+#define DEF_PINNUM_AD3_SDO      (8)
+#define DEF_PINNUM_AD3_CONV     (9)
+
+// LED
+#define DEF_PINNUM_LED0         (28)
+#define DEF_PINNUM_LED1         (27)
+#define DEF_PINNUM_LED2         (22)
+
+// SW
+#define DEF_PINNUM_SW0          (16)
+
+// GPIO
+#define DEF_PINNUM_GPIO20       (20)    // Free (N.C.)
+#define DEF_PINNUM_GPIO21       (21)    // Free (N.C.)
+
+// Battery voltage monitor
+#define DEF_PINNUM_VBAT_MONI    (26)
+
+#endif //__HW_H__
